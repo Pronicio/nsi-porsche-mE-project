d4131e5c908988690c83efef00f525173866d3ad
Pronicio <paolo0803.giometti@gmail.com>
2023-03-11 19:02:07 +0100

    🚧 Working on the home section.

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <div class="logo"></div>
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Popular</a>
+      <a href="#">Featured</a>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss">
+@import '../assets/style/components/header.scss';
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <section id="home">
+    <h1>Choose The Best Car</h1>
+    <h2>Porsche Mission E</h2>
+    <div class="label">
+      <div class="icon-bolt"></div>
+      <h3>Electric car</h3>
+    </div>
+    <img src="../assets/images/car.png" alt="Porsche Mission E" width="380">
+    <div class="perks">
+      <div class="advantage">
+
+      </div>
+      <div class="advantage">
+
+      </div>
+      <div class="advantage">
+
+      </div>
+    </div>
+    <div class="start"></div>
+  </section>
+</template>
+
+<style lang="scss">
+@import '../assets/style/components/home.scss';
+</style>
